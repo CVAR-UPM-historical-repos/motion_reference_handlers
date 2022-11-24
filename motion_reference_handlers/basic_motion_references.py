c2c4b6ae7c986a9c23737c0e87dc9531afb67341
RPS98 <rafa.perez.s98@gmail.com>
2022-11-24 13:24:13 +0100

    Fix multiples instances

--- motion_reference_handlers/basic_motion_references.py
+++ motion_reference_handlers/basic_motion_references.py
@@ -2,7 +2,9 @@
 Implementation of a motion reference handler base.
 """
 
+from dataclasses import dataclass
 import time
+from rclpy.publisher import Publisher
 from rclpy.node import Node, MutuallyExclusiveCallbackGroup
 from rclpy.qos import qos_profile_sensor_data, qos_profile_system_default
 from as2_msgs.msg import ControlMode, ControllerInfo
@@ -32,51 +34,65 @@ class Singleton(type):
         return cls._instances[cls]
 
 
+@dataclass
+class MotionReferenceHandlerBaseData:
+    """ Implementation of a motion reference handler base data """
+    command_pose_pub_: Publisher
+    command_twist_pub_: Publisher
+    command_traj_pub_: Publisher
+
+
 class BasicMotionReferencesBase(metaclass=Singleton):
     """ Implementation of a motion reference handler base for singletons """
+    _instances_list = {}
     number_of_instances_ = -1
-    current_mode_ = ControlMode()
-    controller_info_sub_ = None
-    command_traj_pub_ = None
-    command_pose_pub_ = None
-    command_twist_pub_ = None
 
-    def __init__(self, node: Node):
-        self.number_of_instances_ += 1
+    def __init__(self, _node: Node):
+        if _node in self._instances_list:
+            _node.get_logger().debug(
+                f"Instance of motion reference with {_node.get_namespace()} node already exists")
+            return
 
-        if self.number_of_instances_ == 0:
-            my_callback_group = MutuallyExclusiveCallbackGroup()
-            self.controller_info_sub_ = node.create_subscription(
-                ControllerInfo, as2_names_topic_controller_info,
-                self.controller_info_callback, as2_names_topic_controller_qos, callback_group=my_callback_group)
+        _command_pose_pub = _node.create_publisher(
+            PoseStamped, as2_names_topic_motion_reference_pose,
+            as2_names_topic_motion_reference_qos)
 
-            self.command_pose_pub_ = node.create_publisher(
-                PoseStamped, as2_names_topic_motion_reference_pose,
-                as2_names_topic_motion_reference_qos)
+        _command_twist_pub = _node.create_publisher(
+            TwistStamped, as2_names_topic_motion_reference_twist,
+            as2_names_topic_motion_reference_qos)
 
-            self.command_twist_pub_ = node.create_publisher(
-                TwistStamped, as2_names_topic_motion_reference_twist,
-                as2_names_topic_motion_reference_qos)
+        _command_traj_pub = _node.create_publisher(
+            JointTrajectoryPoint, as2_names_topic_motion_reference_trajectory,
+            as2_names_topic_motion_reference_qos)
 
-            self.command_traj_pub_ = node.create_publisher(
-                JointTrajectoryPoint, as2_names_topic_motion_reference_trajectory,
-                as2_names_topic_motion_reference_qos)
+        data = MotionReferenceHandlerBaseData(
+            command_pose_pub_=_command_pose_pub,
+            command_twist_pub_=_command_twist_pub,
+            command_traj_pub_=_command_traj_pub)
 
-        node.get_logger().debug(
-            f'#{self.number_of_instances_} instances of BasicMotionReferenceHandler created')
+        self._instances_list[_node] = data
+        _node.get_logger().debug(
+            f"Instance of motion reference with {_node.get_namespace()} node created")
 
-    def controller_info_callback(self, msg: ControllerInfo):
-        """ Callback for controller info """
-        self.current_mode_ = msg.input_control_mode
-        return
+    def publish_command_pose(self, _node: Node, _pose: PoseStamped):
+        """ Publish a pose command """
+        self._instances_list[_node].command_pose_pub_.publish(_pose)
+
+    def publish_command_twist(self, _node: Node, _twist: TwistStamped):
+        """ Publish a twist command """
+        self._instances_list[_node].command_twist_pub_.publish(_twist)
+
+    def publish_command_trajectory(self, _node: Node, _trajectory: JointTrajectoryPoint):
+        """ Publish a trajectory command """
+        self._instances_list[_node].command_traj_pub_.publish(_trajectory)
 
 
 class BasicMotionReferenceHandler():
     """ Implementation of a motion reference handler base """
 
     def __init__(self, node: Node):
-        self.motion_handler_ = BasicMotionReferencesBase(node)
 
+        self.motion_handler_ = BasicMotionReferencesBase(node)
         self.node = node
         self.command_trajectory_msg_ = JointTrajectoryPoint()
         self.command_pose_msg_ = PoseStamped()
@@ -86,13 +102,24 @@ class BasicMotionReferenceHandler():
         self.desired_control_mode_.control_mode = ControlMode.UNSET
         self.desired_control_mode_.reference_frame = ControlMode.UNDEFINED_FRAME
 
+        self.current_mode_ = ControlMode()
+        my_callback_group = MutuallyExclusiveCallbackGroup()
+        self.controller_info_sub = node.create_subscription(
+            ControllerInfo, as2_names_topic_controller_info,
+            self.controller_info_callback, as2_names_topic_controller_qos, callback_group=my_callback_group)
+
+    def controller_info_callback(self, msg: ControllerInfo):
+        """ Callback for controller info """
+        self.current_mode_ = msg.input_control_mode
+        return
+
     def check_mode(self) -> bool:
         """ Check if the current mode is the desired mode """
         if (self.desired_control_mode_.control_mode ==
-                self.motion_handler_.current_mode_.control_mode) == ControlMode.HOVER:
+                self.current_mode_.control_mode) == ControlMode.HOVER:
             return True
-        if (self.desired_control_mode_.yaw_mode != self.motion_handler_.current_mode_.yaw_mode or
-                self.desired_control_mode_.control_mode != self.motion_handler_.current_mode_.control_mode):
+        if (self.desired_control_mode_.yaw_mode != self.current_mode_.yaw_mode or
+                self.desired_control_mode_.control_mode != self.current_mode_.control_mode):
             if not self.set_mode(self.desired_control_mode_):
                 return False
         return True
@@ -102,8 +129,8 @@ class BasicMotionReferenceHandler():
         if not self.check_mode():
             return False
         self.command_pose_msg_.header.stamp = self.node.get_clock().now().to_msg()
-        self.motion_handler_.command_pose_pub_.publish(
-            self.command_pose_msg_)
+        self.motion_handler_.publish_command_pose(
+            self.node, self.command_pose_msg_)
         return True
 
     def send_twist_command(self) -> bool:
@@ -111,16 +138,16 @@ class BasicMotionReferenceHandler():
         if not self.check_mode():
             return False
         self.command_twist_msg_.header.stamp = self.node.get_clock().now().to_msg()
-        self.motion_handler_.command_twist_pub_.publish(
-            self.command_twist_msg_)
+        self.motion_handler_.publish_command_twist(
+            self.node, self.command_twist_msg_)
         return True
 
     def send_trajectory_command(self) -> bool:
         """ Send a trajectory command """
         if not self.check_mode():
             return False
-        self.motion_handler_.command_traj_pub_.publish(
-            self.command_trajectory_msg_)
+        self.motion_handler_.publish_command_trajectory(
+            self.node, self.command_trajectory_msg_)
         return True
 
     def set_mode(self, mode: ControlMode) -> bool:
@@ -138,7 +165,7 @@ class BasicMotionReferenceHandler():
         resp = set_control_mode_cli_.call(req)
         if resp.success:
             init_time = self.node.get_clock().now()
-            while self.motion_handler_.current_mode_.control_mode != mode.control_mode:
+            while self.current_mode_.control_mode != mode.control_mode:
                 if (self.node.get_clock().now() - init_time).nanoseconds > 5e9:
                     self.node.get_logger().error(
                         f"Timeout waiting for mode {mode.control_mode}")
